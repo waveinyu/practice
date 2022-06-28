f5785ad77561d8baa70c489a93f8d58377517e62
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-06-28 18:14:57 +0900

    venv 및 Jinja.md 추가

--- registration/venv/Scripts/activate.bat
+++ registration/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\daily\Desktop\Develop\3.hanghae\1.Study\backprac\prac1\signup\venv
+set VIRTUAL_ENV=C:\Users\daily\Desktop\Develop\3.hanghae\1.Study\backprac\practice\registration\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
